36d62f3ca3a73f11b4630b95c9ab0524c166966f
arianTM <88108832+arianTM@users.noreply.github.com>
2023-04-07 23:02:53 -0500

    Building homepage with React

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2022/12/0x0-Charging_06.png?w=990&crop=1"
+            alt="Tesla EV charging"
+          />
+        </div>
+        <div className="texts">
+          <h2>Tesla’s strategy to fuel EV sales? Keep cutting prices</h2>
+          <p className="info">
+            <a className="author">Dawid Paszko</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            Tesla may be a relative newcomer compared to legacy companies like
+            GM and Ford, but it has adopted an age-old tactic in the industry:
+            price wars.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
-
-export default App;
