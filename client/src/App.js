b5386b5d4754ab9c0e01977292d5c936f978abc1
arianTM <88108832+arianTM@users.noreply.github.com>
2023-04-08 00:25:30 -0500

    Login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./components/Layout.js";
-import Post from "./components/Post.js";
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage.js";
 import LoginPage from "./pages/LoginPage.js";
+import RegisterPage from "./pages/RegisterPage.js";
 
 export default function App() {
   return (
@@ -12,6 +12,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
-    <form className="login">
-      <h1>Login</h1>
+    <form className="register">
+      <h1>Register</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-      <button>Login</button>
+      <button>Register</button>
     </form>
   );
 }
